50c003eacaf2c7e264f2861f2c1f467f649ceefe
Alexandre Rose <alexandre.rose.0@gmail.com>
2017-12-28 06:57:53 -0800

    Moving login to use Chromium.
    Some things are broken now, not sure what. Will investigate later.
    Todo: test 2FA and refactor 2FA function to reuse regular auth code

--- scripts/login.js
+++ scripts/login.js
@@ -1,9 +1,6 @@
 const crypt = require('./crypt.js');
+const Puppet = require('./puppet.js');
 const readlineSync = require('readline-sync');
-const phantomjs = require('phantomjs-prebuilt');
-const path = require('path');
-let phantom;
-const DEBUG = true;
 
 Login = function() { };
 
@@ -16,60 +13,44 @@ Login.prototype.execute = function(callback) {
 
     console.log("Attempting login...");
 
-    // This needs to stay "var" for phantomJS
-    // TODO: change phantomJS to pupeteer
-    var arguments = [path.resolve(__dirname, 'phantom.js'), result.email, result.password];
+    
+    const puppet = new Puppet(result.email, result.password);
+    puppet.executeAndGetCookie().then( data => {
+        
+        for (let i = 0; i < data.cookie.length; ++i) {
+            if (data.cookie[i].name === 'c_user') {
+                data.c_user = data.cookie[i].value;
+            }
+        }
 
-    phantom = new login.run_cmd( phantomjs.path, arguments, (err) => {
-        if (err) return callback(err);
-
-        if (phantom.data){
-            let objData;
-            try {
-                objData = JSON.parse(phantom.data);
-            } catch (parseErr) {
-                console.log('Warning: Errors caught in return data'.yellow);
-                if (phantom.data.indexOf('{') !== -1) {
-                    const trimmed = phantom.data.substring(phantom.data.indexOf('{'));
-                    try {
-                        objData = JSON.parse(trimmed);
-                    } catch (err2) {
-                        return callback(err2);
-                    }
-                } else {
-                    return callback(new Error('Invalid phantomJS data'));
-                }
+        let cookie = '';
+        for (let i = 0; i < data.cookie.length; ++i){
+            if (i > 0) {
+                cookie += '; ';
             }
 
-            // Add save time to data
-            objData.saveTime = new Date().getTime();
+            cookie += `${data.cookie[i].name}=${data.cookie[i].value}`;
 
-            // Save user data to file for next login
-            crypt.save(JSON.stringify(objData));
-            return callback();
-
-        } else {
-            return callback(new Error('Bad Facebook credentials'));
+            if (data.cookie[i].name === 'c_user') {
+                data.c_user = data.cookie[i].value;
+            }
         }
+
+        // Replace cookie object with cookie string for HTTP header
+        data.cookie = cookie;
+        
+        // Add save time to data
+        data.saveTime = new Date().getTime();
+        
+        // Save user data to file for next login
+        crypt.save(JSON.stringify(data));
+        console.log(`Saving to crypt ${JSON.stringify(data)}`);
+        
+
+        callback();
+    }).catch(error => {
+        callback(error);
     });
-};
-
-Login.prototype.run_cmd = function(cmd, args, cb) {
-    const spawn = require('child_process').spawn;
-    const child = spawn(cmd, args);
-    const me = this;
-
-    if (DEBUG) child.stdout.pipe(process.stdout);
-
-    child.stderr.pipe(process.stderr);
-    child.stdout.on('data', (buffer) => {
-        if (me.data === undefined) {
-            me.data = buffer.toString();
-        } else {
-            me.data += buffer.toString();
-        }
-    });
-    child.stdout.on('end', cb);
 };
 
 module.exports = Login;
--- scripts/puppet.js
+++ scripts/puppet.js
@@ -1,17 +1,18 @@
 const puppeteer = require('puppeteer');
 
-class Puppet {
+class Puppet  {
     constructor(username, password) {
         this.user = username;
         this.pass = password;
         this.browser;
         this.cookie;
         this.authError;
+        this.fb_value;
     }
 
     executeAndGetCookie() {
         let page;
-        return puppeteer.launch({headless: true}).then(browser => {
+        return puppeteer.launch({headless: false}).then(browser => {
             this.browser = browser;
             return this.browser.pages();
         }).then(promisedPage => {
@@ -28,8 +29,11 @@ class Puppet {
             this.authPromise = new Promise((resolve, reject) => {
                 let attempt = 0;
                 const interval = setInterval(() => {
-                    if (this.cookie) {
-                        resolve(this.cookie);
+                    if (this.cookie && this.fb_value) {
+                        resolve({
+                            cookie: this.cookie,
+                            fb_value: this.fb_value
+                        });
                         clearInterval(interval);
                     } else if (attempt++ >= 40) {
                         reject(`Could not login in time`);
@@ -41,33 +45,12 @@ class Puppet {
                 }, 500);
             });
 
-            page.on('load', () => {
-                return this.browser.pages().then(pages => {
-                    const page = pages[0];
-                    return page.cookies();
-                }).then(cookies => {
-                    return this._getAuthCookie(cookies);
-                }).then(cookie => {
-                    if (cookie) {
-                        this.cookie = cookie;
-                        return Promise.resolve(this.cookie);
-                    } else {
-                        // Check if failure is because of wrong password
-                        return page.evaluate(() => {
-                            return new Promise((resolve, reject) => {
-                                const errorElements = document.getElementsByClassName('_3403 _3404');
-                                if (errorElements.length > 0) {
-                                    reject(new Error(errorElements[0].children[0].innerHTML));
-                                } else {
-                                    reject("Cookie not found. Cause unknown.");
-                                }
-                            });
-                        });
-                    }
-                }).catch(err => {
-                    this.authError = err;
-                });
-            });
+            const pageLoadCallback = this._onPageLoad.bind(this, [false]);
+            page.on('load', pageLoadCallback);
+
+            const pageRequestCallback = this._onPageRequest.bind(this);
+            page.on('request', pageRequestCallback);
+            
             return page.click('#loginbutton');
         }).then(() => {
             console.log(`Waiting 20 seconds for login page`);
@@ -114,20 +97,13 @@ class Puppet {
                     }
                 }, 500);
             });
-            page.on('load', () => {
-                return this.browser.pages().then(pages => {
-                    const page = pages[0];
-                    return page.cookies();
-                }).then(cookies => {
-                    return this._getAuthCookie(cookies);
-                }).then(cookie => {
-                    if (cookie) {
-                        this.cookie = cookie;
-                    }
-                }).catch(() => {
-                    console.log(`Waiting for user to complete 2FA flow`);
-                });
-            });
+
+            const pageLoadCallback = this._onPageLoad.bind(this, [true]);
+            page.on('load', pageLoadCallback);
+
+            const pageRequestCallback = this._onPageRequest.bind(this);
+            page.on('request', pageRequestCallback);
+
             return page.click('#loginbutton');
         }).then(() => {
             console.log(`You have 120 seconds to complete 2FA`);
@@ -152,13 +128,60 @@ class Puppet {
         return new Promise((resolve, reject) => {
             for (let i = 0; i < cookies.length; i++) {
                 if (cookies[i].name === 'c_user') {
-                    resolve(`${cookies[i].name}=${cookies[i].value}`);
+                    resolve(cookies);
                     return;
                 }
             }
 
             resolve(null);
         });
+    }
+
+    _onPageLoad(is2FA) {
+        return this.browser.pages().then(pages => {
+            const page = pages[0];
+            return page.cookies();
+        }).then(cookies => {
+            return this._getAuthCookie(cookies);
+        }).then(cookie => {
+            if (cookie) {
+                this.cookie = cookie;
+                return Promise.resolve(this.cookie);
+            } else {
+                // Check if failure is because of wrong password
+                return page.evaluate(() => {
+                    return new Promise((resolve, reject) => {
+                        const errorElements = document.getElementsByClassName('_3403 _3404');
+                        if (errorElements.length > 0) {
+                            reject(new Error(errorElements[0].children[0].innerHTML));
+                        } else {
+                            reject("Cookie not found. Cause unknown.");
+                        }
+                    });
+                });
+            }
+        }).catch(err => {
+            if (is2FA) {
+                console.log(`Waiting for user to complete 2FA flow`);
+            } else {
+                this.authError = err;
+            }
+        });
+    }
+
+    _onPageRequest(request) {
+        const postData = request.postData;
+
+        if (postData) {
+            let post = postData.split('&');
+            let search = 'fb_dtsg=';
+            for (let i = 0; i < post.length; ++i) {
+                if (post[i].indexOf(search) === 0) {
+                    this.fb_value = decodeURIComponent(post[i].substr(search.length));
+                    break;
+                }
+            }
+        }
     }
 }
 
